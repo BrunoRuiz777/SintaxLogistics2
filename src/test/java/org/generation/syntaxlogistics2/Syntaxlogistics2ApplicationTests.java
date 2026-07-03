a1782725bc2000a795d016a988c69859e14eedff
BrunoRuiz777 <cbm0100608ruiz@gmail.com>
2026-07-03 15:47:31 -0600

    iniciamos repo backend

---
+++ src/test/java/org/generation/syntaxlogistics2/Syntaxlogistics2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.syntaxlogistics2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Syntaxlogistics2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
